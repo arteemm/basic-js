1345b82bbe6e3a1c0b5790b72f2d863d00014621
arteemm <arteemmarteemm@gmail.com>
2021-10-03 19:56:41 +0300

    init: basicjs task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,7 +17,14 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-export default function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+ export default function dateSample(n) {
+  if (typeof n !== 'string' || isNaN(n) || !n || n == false){
+    return false;
+  }
+  if(+n > 15 || +n <=0){
+    return false;
+  }
+ // throw new CustomError('Not implemented');
+let time = Math.log(MODERN_ACTIVITY / +n ) / (0.693 /HALF_LIFE_PERIOD );
+return Math.ceil(time);
 }
--- src/count-cats.js
+++ src/count-cats.js
@@ -14,7 +14,12 @@ import { NotImplementedError } from '../extensions/index.js';
  * ]) => 3`
  *
  */
-export default function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function countCats(arr) {
+  let sum = arr.reduce((count, item) => {
+    let x =  item.filter(item => item === '^^');
+      count += x.length;
+    
+    return count;
+  },0);
+  return sum;
 }
